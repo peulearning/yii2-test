2606bd5a603e05685e8eddcd197526c9716c4f60
Pedro <peuhenry2018@gmail.com>
2024-07-08 21:34:20 -0300

    News Features�

---
+++ views/comment/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use app\Models\Comment;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Comments';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comment-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Comment', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'content:ntext',
+            'post_id',
+            'user_id',
+            'created_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Comment $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
